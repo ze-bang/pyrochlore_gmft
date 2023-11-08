1e008f4befac88b01c47996c239b9e12a3d8401b
Zhengbang Zhou <zhouzb79@cedar1.cedar.computecanada.ca>
2023-11-08 15:55:41 -0800

    some tweaks

--- pyrochlore_dispersion_pi.py
+++ pyrochlore_dispersion_pi.py
@@ -253,7 +253,7 @@ def findminLam(M, K, tol, eta, Jpm, Jpmpm, h, n, theta, chi, chi0, xi):
     E = np.around(E[:,0])
     Em = E.min()
     dex = np.where(E == Em)
-    Know = np.unique(K[dex], axis=0)[0]
+    Know = K[dex][0]
     # print(Know)
     if Know.shape == (3,):
         Know = Know.reshape(1,3)
