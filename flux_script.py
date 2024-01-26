60531b761444ed1b37f00723b77d51341f052bac
Zhengbang Zhou <zhouzb79@nia-login03.scinet.local>
2024-01-26 13:08:20 -0500

    nia update

--- flux_script.py
+++ flux_script.py
@@ -19,9 +19,9 @@ plot_MFE_flux_110(0, 1, -0.08, -0.08, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane
 # plot_MFE_flux_110(0.1, 0.1, 1, 0.25, h110, 2, 40, 101, "h110_flux_plane_pi_0.25")
 # plot_MFE_flux_110(-0.08, -0.08, 1, 0.25, h110, 2, 40, 101, "h110_flux_plane_zero_0.25")
 
-plot_MFE_flux_111(1, -0.001, -0.001, 1, 0.3, h111, 2, 30, 101, "h111_flux_plane_mid_0.3_n1=1")
-plot_MFE_flux_111(1, 0.1, 0.1, 1, 0.3, h111, 2, 30, 101, "h111_flux_plane_pi_0.3_n1=1")
-plot_MFE_flux_111(1, -0.08, -0.08, 1, 0.3, h111, 2, 30 , 101, "h111_flux_plane_zero_0.3_n1=1")
+# plot_MFE_flux_111(1, -0.001, -0.001, 1, 0.3, h111, 2, 30, 101, "h111_flux_plane_mid_0.3_n1=1")
+# plot_MFE_flux_111(1, 0.1, 0.1, 1, 0.3, h111, 2, 30, 101, "h111_flux_plane_pi_0.3_n1=1")
+# plot_MFE_flux_111(1, -0.08, -0.08, 1, 0.3, h111, 2, 30 , 101, "h111_flux_plane_zero_0.3_n1=1")
 
 
 # A = flux_converge_line(-0.4, 0.4, 40, 0.3, h111, 2, 26, 4, "h111_flux_line")
