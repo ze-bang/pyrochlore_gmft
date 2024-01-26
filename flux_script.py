ac1a6fd24d18136df9b15fe187e18c89550a579c
zhengbangzhou <zhouzb79@gmail.com>
2024-01-26 13:06:01 -0500

    stuff

--- flux_script.py
+++ flux_script.py
@@ -10,7 +10,9 @@ from misc_helper import *
 from variation_flux import *
 
 
-# flux_converge_line(-0.05, 0.05, 120, 0.1, h110, 2, 35, 3, 'flux_converge_h110')
+plot_MFE_flux_110(0, 1, -0.001, -0.001, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_mid_0.2_n1=0_n2=1")
+plot_MFE_flux_110(0, 1, 0.1, 0.1, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_pi_0.2_n1=0_n2=1")
+plot_MFE_flux_110(0, 1, -0.08, -0.08, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_zero_0.2_n1=0_n2=1")
 
 
 # plot_MFE_flux_110(-0.001, -0.001, 1, 0.25, h110, 2, 40, 101, "h110_flux_plane_mid_0.25")
