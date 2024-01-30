68e936f07f08ee1340e264ed31a0efe156559671
Zhengbang Zhou <zhouzb79@nia-login03.scinet.local>
2024-01-30 18:59:41 -0500

    nia update

--- variation_flux_script.py
+++ variation_flux_script.py
@@ -10,21 +10,21 @@ from misc_helper import *
 from variation_flux import *
 
 
-plot_MFE_flux_110(0, 0, -0.008, -0.008, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_mid_0.2_n1=0_n2=0")
-plot_MFE_flux_110(0, 0, 0.1, 0.1, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_pi_0.2_n1=0_n2=0")
-plot_MFE_flux_110(0, 0, -0.08, -0.08, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_zero_0.2_n1=0_n2=0")
+plot_MFE_flux_110(0, 0, -0.008, -0.008, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_mid_0.2_n1=0_n2=0")
+plot_MFE_flux_110(0, 0, 0.1, 0.1, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_pi_0.2_n1=0_n2=0")
+plot_MFE_flux_110(0, 0, -0.08, -0.08, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_zero_0.2_n1=0_n2=0")
 
-plot_MFE_flux_110(1, 0, -0.008, -0.008, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_mid_0.2_n1=1_n2=0")
-plot_MFE_flux_110(1, 0, 0.1, 0.1, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_pi_0.2_n1=1_n2=0")
-plot_MFE_flux_110(1, 0, -0.08, -0.08, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_zero_0.2_n1=1_n2=0")
+plot_MFE_flux_110(1, 0, -0.008, -0.008, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_mid_0.2_n1=1_n2=0")
+plot_MFE_flux_110(1, 0, 0.1, 0.1, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_pi_0.2_n1=1_n2=0")
+plot_MFE_flux_110(1, 0, -0.08, -0.08, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_zero_0.2_n1=1_n2=0")
 
-plot_MFE_flux_110(0, 1, -0.008, -0.008, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_mid_0.2_n1=0_n2=1")
-plot_MFE_flux_110(0, 1, 0.1, 0.1, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_pi_0.2_n1=0_n2=1")
-plot_MFE_flux_110(0, 1, -0.08, -0.08, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_zero_0.2_n1=0_n2=1")
+plot_MFE_flux_110(0, 1, -0.008, -0.008, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_mid_0.2_n1=0_n2=1")
+plot_MFE_flux_110(0, 1, 0.1, 0.1, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_pi_0.2_n1=0_n2=1")
+plot_MFE_flux_110(0, 1, -0.08, -0.08, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_zero_0.2_n1=0_n2=1")
 
-plot_MFE_flux_110(1, 1, -0.008, -0.008, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_mid_0.2_n1=1_n2=1")
-plot_MFE_flux_110(1, 1, 0.1, 0.1, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_pi_0.2_n1=1_n2=1")
-plot_MFE_flux_110(1, 1, -0.08, -0.08, 1, 0.2, h110, 2, 30, 101, "h110_flux_plane_zero_0.2_n1=1_n2=1")
+plot_MFE_flux_110(1, 1, -0.008, -0.008, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_mid_0.2_n1=1_n2=1")
+plot_MFE_flux_110(1, 1, 0.1, 0.1, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_pi_0.2_n1=1_n2=1")
+plot_MFE_flux_110(1, 1, -0.08, -0.08, 1, 0.2, h110, 2, 40, 101, "h110_flux_plane_zero_0.2_n1=1_n2=1")
 
 # flux_converge_line(-0.05, 0.05, 50, 0.1, h110, 2, 35, 3, 'flux_converge_h110')
 # flux_converge_line(-0.05, 0.05, 50, 0.1, h111, 2, 35, 3, 'flux_converge_h111')
