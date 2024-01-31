a4e30e34d7c14fa5861728eaa75b92bb19e2167e
Zhengbang Zhou <zhouzb79@nia-login03.scinet.local>
2024-01-30 22:24:51 -0500

    nia update

--- flux_script.py
+++ flux_script.py
@@ -10,10 +10,10 @@ from misc_helper import *
 from variation_flux import *
 
 
-plot_MFE_flux_111(0, -0.008, -0.008, 1, 0.2, h111, 2, 30, 101, "h111_flux_plane_mid_0.2_n1=0")
-plot_MFE_flux_111(0, 0.1, 0.1, 1, 0.2, h111, 2, 30, 101, "h111_flux_plane_pi_0.2_n1=0_n2=0")
-plot_MFE_flux_111(0, -0.08, -0.08, 1, 0.2, h111, 2, 30, 101, "h111_flux_plane_zero_0.2_n1=0_n2=0")
+plot_MFE_flux_111(0, -0.008, -0.008, 1, 0.3, h111, 2, 40, 101, "h111_flux_plane_mid_0.3_n1=0")
+plot_MFE_flux_111(0, 0.1, 0.1, 1, 0.3, h111, 2, 40, 101, "h111_flux_plane_pi_0.3_n1=0_n2=0")
+plot_MFE_flux_111(0, -0.05, -0.05, 1, 0.3, h111, 2, 40, 101, "h111_flux_plane_zero_0.3_n1=0_n2=0")
 
-plot_MFE_flux_111(1, -0.008, -0.008, 1, 0.2, h111, 2, 30, 101, "h111_flux_plane_mid_0.2_n1=1")
-plot_MFE_flux_111(1, 0.1, 0.1, 1, 0.2, h111, 2, 30, 101, "h111_flux_plane_pi_0.2_n1=1")
-plot_MFE_flux_111(1, -0.08, -0.08, 1, 0.2, h111, 2, 30, 101, "h111_flux_plane_zero_0.2_n1=1")
+plot_MFE_flux_111(1, -0.008, -0.008, 1, 0.3, h111, 2, 40, 101, "h111_flux_plane_mid_0.3_n1=1")
+plot_MFE_flux_111(1, 0.1, 0.1, 1, 0.3, h111, 2, 40, 101, "h111_flux_plane_pi_0.3_n1=1")
+plot_MFE_flux_111(1, -0.05, -0.05, 1, 0.3, h111, 2, 40, 101, "h111_flux_plane_zero_0.3_n1=1")
