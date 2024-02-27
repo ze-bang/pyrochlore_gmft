3ad22ae3442ebb200b90ca5038e248290ffa499c
zhengbangzhou <zhouzb79@gmail.com>
2024-02-27 11:21:49 -0500

    some better fix for completeSpan

--- variation_flux_script.py
+++ variation_flux_script.py
@@ -9,10 +9,10 @@ completeSpan(-0.1,0.1,100,0,0.3,100,h110,30,2,np.ones(4)*np.pi,'HanYan_110_Jpm_-
 completeSpan(-0.1,0.1,100,0,0.3,100,h110,30,2,np.array([np.pi, np.pi, 0,0]),'HanYan_110_Jpm_-0.1_0.1_h_0_0.3_pipi00')
 completeSpan(-0.1,0.1,100,0,0.3,100,h110,30,2,np.array([0,0,np.pi,np.pi]),'HanYan_Jpm_110_-0.1_0.1_h_0_0.3_0_00pipi')
 
-completeSpan(-0.1,0.1,100,0,0.3,100,h100,30,2,np.zeros(4),'HanYan_100_Jpm_-0.1_0.1_h_0_0.3_0_flux')
-completeSpan(-0.1,0.1,100,0,0.3,100,h100,30,2,np.ones(4)*np.pi,'HanYan_100_Jpm_-0.1_0.1_h_0_0.3_pi_flux')
-completeSpan(-0.1,0.1,100,0,0.3,100,h100,30,2,np.array([np.pi, 0,0, np.pi]),'HanYan_100_Jpm_-0.1_0.1_h_0_0.3_0pipi0')
-completeSpan(-0.1,0.1,100,0,0.3,100,h100,30,2,np.array([0,np.pi,np.pi,0]),'HanYan_100_Jpm_-0.1_0.1_h_0_0.3_0_pi00pi')
+completeSpan(-0.1,0.1,100,0,0.3,100,h001,30,2,np.zeros(4),'HanYan_100_Jpm_-0.1_0.1_h_0_0.3_0_flux')
+completeSpan(-0.1,0.1,100,0,0.3,100,h001,30,2,np.ones(4)*np.pi,'HanYan_100_Jpm_-0.1_0.1_h_0_0.3_pi_flux')
+completeSpan(-0.1,0.1,100,0,0.3,100,h001,30,2,np.array([np.pi, 0,0, np.pi]),'HanYan_100_Jpm_-0.1_0.1_h_0_0.3_0pipi0')
+completeSpan(-0.1,0.1,100,0,0.3,100,h001,30,2,np.array([0,np.pi,np.pi,0]),'HanYan_100_Jpm_-0.1_0.1_h_0_0.3_0_pi00pi')
 
 completeSpan(-0.1,0.1,100,0,0.3,100,h111,30,2,np.zeros(4),'HanYan_111_Jpm_-0.1_0.1_h_0_0.3_0_flux')
 completeSpan(-0.1,0.1,100,0,0.3,100,h111,30,2,np.ones(4)*np.pi,'HanYan_111_Jpm_-0.1_0.1_h_0_0.3_pi_flux')
