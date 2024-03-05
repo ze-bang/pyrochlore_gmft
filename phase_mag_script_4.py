474d84f70b24dd05bef6169a61818003bccfad1d
Zhengbang Zhou <zhouzb79@nia-login05.scinet.local>
2024-03-05 01:39:57 -0500

    nia update fix

--- phase_mag_script_4.py
+++ phase_mag_script_4.py
@@ -6,4 +6,4 @@ from misc_helper import *
 flux = np.array([np.pi, np.pi, 0, 0])
 
 findPhaseMag110(-0.5, 0.1, 100, 0, 2, 100, h110, 30, 2, "phase_110_kappa=2")
-# findPhaseMag110_ex(-0.5, 0.1, 100, 0, 2, 100, h110, 17, 2, "phase_110_kappa=2_ex")
+# findPhaseMag110_ex(-0.5, 0.1, 100, 0, 1, 100, h110, 17, 2, "phase_110_kappa=2_ex")
