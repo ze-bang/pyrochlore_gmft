eb00c257ed0b17b661e5711dc695a98f84133478
Zhengbang Zhou <zhouzb79@nia-login05.scinet.local>
2024-03-13 15:08:16 -0400

    nia update

--- monte_carlo.py
+++ monte_carlo.py
@@ -429,10 +429,10 @@ def scan_line(dirname, Jxx, Jyy, Jzz, hmin, hmax, nScan, n, gx, gy, gz, d, Targe
         monte_SSSF(filename, Jxx, Jyy, Jzz, currH[i], n, gx, gy, gz, d, Target, Tinit, ntemp, nsweep)
 
 def scan_all(n, L):
-    scan_line('./monte_carlo_files/Jpm_0.3/', -0.6, 1, -0.6, 0, 1, 20, n, 0, 0, 1, L, -7, 1, 200, 100000)
-    scan_line('./monte_carlo_files/Jpm_-0.3/', 0.6, 1, 0.6, 0, 1, 20, n, 0, 0, 1, L, -7, 1, 200, 100000)
-    scan_line('./monte_carlo_files/Jpm_0.1/', -0.2, 1, -0.2, 0, 1, 20, n, 0, 0, 1, L, -7, 1, 200, 100000)
-    scan_line('./monte_carlo_files/Jpm_-0.1/', 0.2, 1, 0.2, 0, 1, 20, n, 0, 0, 1, L, -7, 1, 200, 100000)
+    scan_line('./monte_carlo_files/Jpm_0.3/', -0.6, 1, -0.6, 0, 1, 40, n, 0, 0, 1, L, -7, 1, 200, 100000)
+    scan_line('./monte_carlo_files/Jpm_-0.3/', 0.6, 1, 0.6, 0, 1, 40, n, 0, 0, 1, L, -7, 1, 200, 100000)
+    scan_line('./monte_carlo_files/Jpm_0.1/', -0.2, 1, -0.2, 0, 1, 40, n, 0, 0, 1, L, -7, 1, 200, 100000)
+    scan_line('./monte_carlo_files/Jpm_-0.1/', 0.2, 1, 0.2, 0, 1, 40, n, 0, 0, 1, L, -7, 1, 200, 100000)
 
 
 
---
+++ monte_carlo_script_1.py
@@ -0,0 +1,7 @@
+from monte_carlo import *
+import os 
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/configs/"
+
+# scan_all(h111, 4)
+scan_all(h110, 4)
+# scan_all(h001, 4)
