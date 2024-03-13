56e6433dd46a47a27fb32b7a0eb14bd9b4e5bb51
Zhengbang Zhou <zhouzb79@nia-login07.scinet.local>
2024-03-13 11:36:17 -0400

    nia update

--- monte_carlo_script.py
+++ monte_carlo_script.py
@@ -1,5 +1,7 @@
 from monte_carlo import *
+import os 
+os.environ['MPLCONFIGDIR'] = os.getcwd() + "/configs/"
 
 scan_all(h111, 4)
-scan_all(h110, 4)
-scan_all(h001, 4)
+# scan_all(h110, 4)
+# scan_all(h001, 4)
