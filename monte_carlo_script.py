7977b5749227b01f0b65344fad464f839cf5c0ee
Zhengbang Zhou <zhouzb79@nia-login01.scinet.local>
2024-03-19 13:59:09 -0400

    nia update

--- monte_carlo_script.py
+++ monte_carlo_script.py
@@ -5,7 +5,7 @@ from phase_diagram import *
 # scan_all(h110, 2)
 # scan_all(h001, 2)
 
-completeSpan_ex(-0.3,0.1,100,0,1,100,h001,30,2,np.zeros(4),'HanYan_100_Jpm_-0.3_0.1_h_0_1_0_flux_ex')
-completeSpan_ex(-0.3,0.1,100,0,1,100,h001,30,2,np.ones(4)*np.pi,'HanYan_100_Jpm_-0.3_0.1_h_0_1_pi_flux_ex')
-completeSpan_ex(-0.3,0.1,100,0,1,100,h001,30,2,np.array([np.pi, 0,0, np.pi]),'HanYan_100_Jpm_-0.3_0.1_h_0_1_0pipi0_ex')
-completeSpan_ex(-0.3,0.1,100,0,1,100,h001,30,2,np.array([0,np.pi,np.pi,0]),'HanYan_100_Jpm_-0.3_0.1_h_0_1_0_pi00pi_ex')
+completeSpan_ex(-0.3,0.1,100,0,1,100,h001,17,2,np.zeros(4),'HanYan_100_Jpm_-0.3_0.1_h_0_1_0_flux_ex')
+completeSpan_ex(-0.3,0.1,100,0,1,100,h001,17,2,np.ones(4)*np.pi,'HanYan_100_Jpm_-0.3_0.1_h_0_1_pi_flux_ex')
+completeSpan_ex(-0.3,0.1,100,0,1,100,h001,17,2,np.array([np.pi, 0,0, np.pi]),'HanYan_100_Jpm_-0.3_0.1_h_0_1_0pipi0_ex')
+completeSpan_ex(-0.3,0.1,100,0,1,100,h001,17,2,np.array([0,np.pi,np.pi,0]),'HanYan_100_Jpm_-0.3_0.1_h_0_1_0_pi00pi_ex')
