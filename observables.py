c8bf71aa816f00c92b6c30a9576667d95cdbe317
zhengbangzhou <zhouzb79@gmail.com>
2024-03-21 13:31:01 -0400

    tested all SSSF

--- observables.py
+++ observables.py
@@ -129,6 +129,8 @@ def SpmSpp(K, Q, q, pyp0, lam=0):
 def SSSF_core(q, v, pyp0):
     Ks = pyp0.pts
     Qs = Ks - q
+    if v.shape == (3,):
+        v = v.reshape((1,3))
     v = contract('ik,i->ik',v, 1/np.linalg.norm(v,axis=1))
 
     Spm, Spp= SpmSpp(Ks, Qs, q, pyp0, lam=pyp0.lams)
@@ -138,10 +140,10 @@ def SSSF_core(q, v, pyp0):
 
     qreal = contract('j,jk->k',q, BasisBZA)
     Sglobalzz = contract('ijk,jk,i->', Szz, g(qreal), pyp0.weights)
-    SNSFzz = contract('ijk,jk,i->', Szz, gNSF(v), pyp0.weights)
+    SNSFzz = contract('ijk,ijk,i->', Szz, gNSF(v), pyp0.weights)
     Szz = contract('ijk,i->', Szz, pyp0.weights)
     Sglobalxx = contract('ijk,jk,i->', Sxx, g(qreal), pyp0.weights)
-    SNSFxx = contract('ijk,jk,i->', Sxx, gNSF(v), pyp0.weights)
+    SNSFxx = contract('ijk,ijk,i->', Sxx, gNSF(v), pyp0.weights)
     Sxx = contract('ijk,i->', Sxx, pyp0.weights)
 
     return Szz, Sglobalzz, SNSFzz, Sxx, Sglobalxx, SNSFxx
--- phase_mag_script_1.py
+++ phase_mag_script_1.py
@@ -23,12 +23,12 @@ from observables import *
 # plotLinefromnetCDF(h001, "h001=0.3_largeN.pdf", h=0.3, diff=True)
 
 Jpm = -0.03
-# py0s = pycon.piFluxSolver(-2*Jpm, -2*Jpm, 1, BZres=25, h=0.3, n=h110, flux=np.ones(4)*np.pi)
-# py0s.solvemeanfield()
-# print(SSSF_core(np.array([0,0,0]), hb110, py0s))
-# print(SSSF_core(np.array([1,1,-1]), hb110, py0s))
+py0s = pycon.piFluxSolver(-2*Jpm, -2*Jpm, 1, BZres=25, h=0.3, n=h111, flux=np.ones(4)*np.pi)
+py0s.solvemeanfield()
+print(SSSF_core(np.array([0.5,1,0.5]), hb110, py0s))
+print(SSSF_core(np.array([1,1,1]), hb110, py0s))
 #
-SSSF(20, -2*Jpm, -2*Jpm, 1, 0.3, h110, np.ones(4)*np.pi,30, "test")
+# SSSF(20, -2*Jpm, -2*Jpm, 1, 0.3, h110, np.ones(4)*np.pi,30, "test")
 #
 # SSSF_line(100, -2*Jpm, -2*Jpm, 1, 0, 0.5, 10, h111, hb110, np.zeros(4),30, "Files/SSSF/Jpm=0.02_0")
 # SSSF_line(100, -2*Jpm, -2*Jpm, 1, 0, 0.5, 10, h111, hb110, np.ones(4)*np.pi, 30, "Files/SSSF/Jpm=0.02_pi")
