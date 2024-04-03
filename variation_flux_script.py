95199d35a01a738c9f0683a7f395dbcf9109d3c4
Zhengbang Zhou <zhouzb79@nia-login06.scinet.local>
2024-04-03 14:44:06 -0400

    job script update

--- variation_flux_script.py
+++ variation_flux_script.py
@@ -21,13 +21,14 @@ from observables import *
 # completeSpan(-0.1,0.1,100,0,0.3,100,h111,30,2,np.ones(4)*np.pi,'HanYan_111_Jpm_-0.1_0.1_h_0_0.3_pi_flux')
 
 Jpm = 0.02
-# SSSF_line(100, -2*Jpm, -2*Jpm, 1, 0, 0.5, 40, h001, np.zeros(4),30, "Files/SSSF/Jpm=5e-3_0")
-# SSSF_line(100, -2*Jpm, -2*Jpm, 1, 0, 0.5, 40, h001, np.ones(4)*np.pi, 30, "Files/SSSF/Jpm=5e-3_pi")
+
+SSSF_line(100, -2*Jpm, -2*Jpm, 1, 0, 0.4, 10, h001, np.zeros(4),30, "Files/SSSF/Jpm=0.02_0")
+SSSF_line(100, -2*Jpm, -2*Jpm, 1, 0, 0.4, 10, h001, np.ones(4)*np.pi, 30, "Files/SSSF/Jpm=0.02_pi")
 #
 # DSSF_line(5e-3, -2*Jpm, -2*Jpm, 1, 0, 0.5, 10, h001, np.zeros(4),30, "Files/DSSF/Jpm=5e-3_0")
 # DSSF_line(5e-3, -2*Jpm, -2*Jpm, 1, 0, 0.5, 10, h001, np.ones(4)*np.pi, 30, "Files/DSSF/Jpm=5e-3_pi")
 
 
-DSSF(0.005, -2*Jpm, -2*Jpm, 1, 0, h001, np.zeros(4), 30, "Files/DSSF/Jpm=0.02_h001=0")
-DSSF(0.005, -2*Jpm, -2*Jpm, 1, 0.15, h001, np.zeros(4), 30, "Files/DSSF/Jpm=0.02_h001=0.15")
-DSSF(0.005, -2*Jpm, -2*Jpm, 1, 0.3, h001, np.zeros(4), 30, "Files/DSSF/Jpm=0.02_h001=0.3")
+# DSSF(0.005, -2*Jpm, -2*Jpm, 1, 0, h001, np.zeros(4), 30, "Files/DSSF/Jpm=0.02_h001=0")
+# DSSF(0.005, -2*Jpm, -2*Jpm, 1, 0.15, h001, np.zeros(4), 30, "Files/DSSF/Jpm=0.02_h001=0.15")
+# DSSF(0.005, -2*Jpm, -2*Jpm, 1, 0.3, h001, np.zeros(4), 30, "Files/DSSF/Jpm=0.02_h001=0.3")
