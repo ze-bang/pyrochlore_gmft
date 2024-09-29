eb3d56b05cb05bb4762b826ad2fa2cce00281ab5
zhengbangzhou <zhouzb79@gmail.com>
2024-09-29 11:53:35 -0400

    incorporate condensate contribution

--- phase_XYZ_collective.py
+++ phase_XYZ_collective.py
@@ -6,7 +6,19 @@ os.environ['MPLCONFIGDIR'] = os.getcwd() + "/configs/"
 from phase_diagram import *
 import sys
 
-Jpm, Jpmax, Jpm1, Jpm1max, nK, nS, flux_string, filename = sys.argv[1:]
+nK, nS, flux_string, SLURM_ID, SLURM_SECTIONS = sys.argv[1:]
+
+Jpm_section = int(np.sqrt(int(SLURM_SECTIONS)))
+Jpm_length = int(SLURM_ID) // Jpm_section
+Jpm_width = int(SLURM_ID) % Jpm_section
+
+Jpm_unit = 2/Jpm_section
+
+Jpm_length_start = -1 + Jpm_length * Jpm_unit
+Jpm_length_end = -1 + (Jpm_length+1) * Jpm_unit
+Jpm_width_start = -1 + Jpm_width * Jpm_unit
+Jpm_width_end = -1 + (Jpm_width+1) * Jpm_unit
+
 flux = ""
 if flux_string == "0":
     flux = np.zeros(4)
@@ -14,5 +26,6 @@ elif flux_string == "pi":
     flux = np.ones(4)*np.pi
 else:
     flux = zppz
-
-findXYZPhase_separate(float(Jpm), float(Jpmax), float(Jpm1), float(Jpm1max), int(nK), 30, 2, flux, filename, int(nS), symmetrized=False)
+print(Jpm_length_start, Jpm_length_end, Jpm_width, Jpm_width, flux, nS)
+filename_here = "pyrochlore_xyz_" + flux_string + "_" + nS + "_" + SLURM_ID + "_out_of_" + SLURM_SECTIONS
+findXYZPhase_separate(float(Jpm_length_start), float(Jpm_length_end), float(Jpm_width_start), float(Jpm_width_end), int(nK), 30, 2, flux, filename_here, int(nS), symmetrized=False)
