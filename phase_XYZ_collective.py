055a14f4161fd4ed9c213e3988bcd3bac1035e4a
zhengbangzhou <zhouzb79@gmail.com>
2024-09-29 11:13:41 -0400

    incorporate condensate contribution

--- phase_XYZ_collective.py
+++ phase_XYZ_collective.py
@@ -15,4 +15,4 @@ elif flux_string == "pi":
 else:
     flux = zppz
 
-findXYZPhase_separate(Jpm, Jpmax, Jpm1, Jpm1max, nK, 30, 2, flux, filename, nS, symmetrized=False)
+findXYZPhase_separate(float(Jpm), float(Jpmax), float(Jpm1), float(Jpm1max), int(nK), 30, 2, flux, filename, int(nS), symmetrized=False)
