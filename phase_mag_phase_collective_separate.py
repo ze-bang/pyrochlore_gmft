e32274ba772bccff83ee7194b5747c092c6a37ee
Zhengbang Zhou <zhouzb79@cedar1.int.cedar.computecanada.ca>
2024-10-10 22:36:21 -0700

    stuff

--- phase_mag_phase_collective_separate.py
+++ phase_mag_phase_collective_separate.py
@@ -5,18 +5,39 @@ from phase_diagram import *
 from observables import *
 import sys
 
-hmin, hmax, field_dir, flux_String, Jpmpm, SLURM_ID, SLURM_SIZE, mpi_size = sys.argv[1:]
+hmin, hmax, field_dir, Jpmpm, SLURM_ID, SLURM_SIZE, mpi_size = sys.argv[1:]
 
-SLURM_ID = int(SLURM_ID) - 1
+if field_dir == "110":
+    h_dir = h110
+    JPMH_PARAM_SIZE = int(SLURM_SIZE) / 4
+elif field_dir == "111":
+    h_dir = h111
+    JPMH_PARAM_SIZE = int(SLURM_SIZE) / 2
+else:
+    h_dir = h001
+    JPMH_PARAM_SIZE = int(SLURM_SIZE) / 2
+
+JOB_ID = int(SLURM_ID) - 1
+flux_ind_ns_ind = JOB_ID // JPMH_PARAM_SIZE
+JPMH_SECTION_ID = JOB_ID % JPMH_PARAM_SIZE
+
+if flux_ind_ns_ind == 0:
+    flux = np.ones(4)*np.pi
+elif flux_ind_ns_ind == 1:
+    flux = np.zeros(4)
+elif flux_ind_ns_ind == 2:
+    flux = zppz
+else:
+    flux = pzzp
 
 hmin = float(hmin)
 hmax = float(hmax)
 Jpmpm = float(Jpmpm)
 
 Jpm_min = -0.5 + abs(float(Jpmpm))
-Jpm_section = int(np.sqrt(int(SLURM_SIZE)))
-Jpm_length = int(SLURM_ID) // Jpm_section
-h_length = int(SLURM_ID) % Jpm_section
+Jpm_section = int(np.sqrt(int(JPMH_PARAM_SIZE)))
+Jpm_length = int(JPMH_SECTION_ID) // Jpm_section
+h_length = int(JPMH_SECTION_ID) % Jpm_section
 
 Jpm_unit = (0.1 - Jpm_min)/Jpm_section
 h_unit = (hmax - hmin) / Jpm_section
@@ -28,22 +49,7 @@ h_length_end = hmin + (h_length+1) * h_unit
 
 filename = "pyrochlore_mag_phase_" + str(field_dir) + "/Jpm_" + str(Jpm_length_start) + "_" + str(Jpm_length_end) + "_h_" + str(h_length_start) + "_" + str(h_length_end) + "_" + str(SLURM_ID) + "_out_of_" + str(SLURM_SIZE)
 
-if field_dir == "110":
-    h_dir = h110
-elif field_dir == "111":
-    h_dir = h111
-else:
-    h_dir = h001
-
-if flux_String == "pi":
-    flux = np.ones(4)*np.pi
-elif flux_String == "0":
-    flux = np.zeros(4)
-elif flux_String == "0pp0":
-    flux = zppz
-else:
-    flux = pzzp
 
 nK = int(np.sqrt(int(mpi_size)))
 
-findPhaseMag_separate(Jpm_length_start, Jpm_length_end, nK, h_length_start, h_length_end, nK, flux, 30, 2, filename, Jpmpm=Jpmpm)
+findPhaseMag_separate(Jpm_length_start, Jpm_length_end, nK, h_length_start, h_length_end, nK, h_dir, flux, 30, 2, filename, Jpmpm=Jpmpm)
